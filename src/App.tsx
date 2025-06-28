9efb4e8b41f6ae4d9830ca9afb77f973408d156b
Olufunke Kayode <olufunkekay1@gmail.com>
2025-06-28 18:21:43 -0500

    updated html

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ function App() {
   return (
     <>
       <h1>Hello World!</h1>
+      <h2>This is a new addition to the node file</h2>
+      <p>Add this new commit to the new branch</p>
 
     </>
   )
