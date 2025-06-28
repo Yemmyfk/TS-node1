424231782d0afa27365d33e72aaf74ab24417425
Olufunke Kayode <olufunkekay1@gmail.com>
2025-06-28 18:10:20 -0500

    node commit

---
+++ src/components/mainstyle.js
@@ -0,0 +1,40 @@
+// import React from 'react'
+// import { Http2ServerRequest } from 'http2';
+
+// const mainstyle = () => {
+
+//     var http = require("http");
+
+//     http.createServer(function (request, response) {
+//    // Send the HTTP header 
+//    // HTTP Status: 200 : OK
+//    // Content Type: text/plain
+//    response.writeHead(200, {'Content-Type': 'text/plain'});
+   
+//    // Send the response body as "Hello World"
+//    response.end('Hello World\n');
+// }).listen(8081);
+
+// // // Console will print the message
+// // console.log('Server running at http://127.0.0.1:8081/');
+    
+
+//   return (
+//    console.log('http'),
+    
+    
+    
+//      <>
+    
+    
+    
+    
+    
+    
+//     </>
+//   )
+// }
+
+// export default mainstyle
+
+var express = require('express');
